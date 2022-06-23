e38a139bf56454d1bfc3d301a7346316766a7bb3
向阳 <xiangqian_liu@163.com>
2022-06-23 11:22:55 +0800

    feat: 画一个三角形

--- canvas/index.js
+++ canvas/index.js
@@ -1,6 +1,6 @@
 const container = document.getElementsByClassName('draw-container')[0]
 
-function drawer() {
+function drawerRect() {
   const ctx = container.getContext('2d')
 
   ctx.fillStyle = 'rgba(200, 100, 100)'
@@ -9,4 +9,19 @@ function drawer() {
   ctx.strokeRect(80, 80, 80, 80)
 }
 
-drawer()
+// drawerRect()
+
+function drawTriangle() {
+  const context = container.getContext('2d')
+  context.beginPath()
+  // 起点
+  context.moveTo(50, 50)
+  // 移动画笔，画出线条
+  context.lineTo(50, 100)
+  context.lineTo(100, 50)
+  context.lineTo(50, 50)
+  // 填充图形
+  context.fill()
+}
+
+// drawTriangle()
