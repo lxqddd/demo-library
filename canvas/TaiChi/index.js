67b362d1abaac8df083ff285607f34d8c8904b73
向阳 <xiangqian_liu@163.com>
2022-06-29 14:34:17 +0800

    feat: 乱掉了

--- canvas/TaiChi/index.js
+++ canvas/TaiChi/index.js
@@ -14,9 +14,23 @@ function drawTaiChi() {
   context.closePath()
 
   context.beginPath()
+  context.moveTo(0, -100)
+  context.fillStyle = '#000'
+  context.arc(0, -50, 50, (Math.PI / 180) * 90, (Math.PI / 180) * -90, true)
+  context.fill()
+  context.closePath()
+
+  context.beginPath()
   context.fillStyle = '#fff'
   context.moveTo(0, -50)
   context.arc(0, -50, 25, 0, Math.PI * 2, false)
+  context.fill()
+  context.closePath()
+
+  context.beginPath()
+  context.arc(0, 0, 100, (Math.PI / 180) * 90, (Math.PI / 180) * -90, true)
+  context.moveTo(0, 100)
+  context.arc(0, 50, 50, (Math.PI / 180) * 90, (Math.PI / 180) * -90, false)
   context.fill()
   context.closePath()
 }
