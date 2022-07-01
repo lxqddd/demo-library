d6550f3a6ca8ca30a15b15aa8e6aee2a595558c3
向阳 <xiangqian_liu@163.com>
2022-07-01 11:23:14 +0800

    feat: 添加阴影

--- canvas/TaiChi/index.js
+++ canvas/TaiChi/index.js
@@ -1,22 +1,27 @@
 const draw = document.getElementById('draw')
 const context = draw.getContext('2d')
 
-function drawCircle(params = { context, x, y, radius, anticlockwise }) {
-  const { context, x, y, radius, anticlockwise } = params
-  context.save()
-  context.arc(x, y, radius, 0, (Math.PI / 180) * 2, anticlockwise)
-}
-
 function drawTaiChi() {
   context.save()
   context.beginPath()
   context.strokeStyle = '#fff'
+  context.shadowBlur = 5
+  context.shadowColor = '#fff'
+
+  context.arc(0, 0, 100, 0, Math.PI2)
+  context.closePath()
+  context.stroke()
+  // context.restore()
+
+  context.beginPath()
+  context.fillStyle = '#000'
+
   context.arc(0, 0, 100, (Math.PI / 180) * 90, (Math.PI / 180) * -90, false)
   context.moveTo(0, 100)
   context.arc(0, 50, 50, (Math.PI / 180) * 90, (Math.PI / 180) * -90, false)
   context.moveTo(0, -100)
   context.arc(0, -50, 50, (Math.PI / 180) * -90, (Math.PI / 180) * 90, false)
-  context.stroke()
+  context.fill()
   context.closePath()
 
   context.beginPath()
