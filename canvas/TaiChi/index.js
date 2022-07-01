55025f716d686481d79768d21b677e2a98d09096
向阳 <xiangqian_liu@163.com>
2022-07-01 13:49:48 +0800

    feat: 太极图画完了

--- canvas/TaiChi/index.js
+++ canvas/TaiChi/index.js
@@ -18,9 +18,9 @@ function drawTaiChi() {
   drawStrokeCircle(context, 100)
 
   context.beginPath()
-  context.fillStyle = '#000'
+  context.fillStyle = '#fff'
 
-  context.arc(0, 0, 100, (Math.PI / 180) * 90, (Math.PI / 180) * -90, false)
+  context.arc(0, 0, 100, (Math.PI / 180) * 90, (Math.PI / 180) * -90, true)
   context.moveTo(0, 100)
   context.arc(0, 50, 50, (Math.PI / 180) * 90, (Math.PI / 180) * -90, false)
   context.moveTo(0, -100)
@@ -28,26 +28,26 @@ function drawTaiChi() {
   context.fill()
   context.closePath()
 
-  context.beginPath()
-  context.moveTo(0, -100)
-  context.fillStyle = '#000'
-  context.arc(0, -50, 50, (Math.PI / 180) * 90, (Math.PI / 180) * -90, true)
-  context.fill()
-  context.closePath()
+  // context.beginPath()
+  // context.moveTo(0, -100)
+  // context.fillStyle = '#000'
+  // context.arc(0, -50, 50, (Math.PI / 180) * 90, (Math.PI / 180) * -90, true)
+  // context.fill()
+  // context.closePath()
 
-  context.beginPath()
-  context.fillStyle = '#fff'
-  context.moveTo(0, -50)
-  context.arc(0, -50, 25, 0, Math.PI * 2, false)
-  context.fill()
-  context.closePath()
+  // context.beginPath()
+  // context.fillStyle = '#fff'
+  // context.moveTo(0, -50)
+  // context.arc(0, -50, 25, 0, Math.PI * 2, false)
+  // context.fill()
+  // context.closePath()
 
-  context.beginPath()
-  context.arc(0, 0, 100, (Math.PI / 180) * 90, (Math.PI / 180) * -90, true)
-  context.moveTo(0, 100)
-  context.arc(0, 50, 50, (Math.PI / 180) * 90, (Math.PI / 180) * -90, false)
-  context.fill()
-  context.closePath()
+  // context.beginPath()
+  // context.arc(0, 0, 100, (Math.PI / 180) * 90, (Math.PI / 180) * -90, true)
+  // context.moveTo(0, 100)
+  // context.arc(0, 50, 50, (Math.PI / 180) * 90, (Math.PI / 180) * -90, false)
+  // context.fill()
+  // context.closePath()
 }
 
 function drawer() {
