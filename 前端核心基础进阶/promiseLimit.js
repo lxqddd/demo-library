f1a909491c1f64a0819cecc0a0080909b7074462
向阳 <xiangqian_liu@163.com>
2022-07-25 10:00:06 +0800

    feat: add some comment

--- 前端核心基础进阶/promiseLimit.js
+++ 前端核心基础进阶/promiseLimit.js
@@ -1,11 +1,14 @@
 const loadByLimit = (urlIds, loadImg, limit) => {
+  // 为了不改变原数组，在这里copy一份
   const urlIdsCopy = [...urlIds]
 
+  // 如果当前数组长度小于限定的长度，直接通过promise.all将所有的请求一起发起
   if (urlIdsCopy.length <= limit) {
     const promiseArr = urlIds.map(urlId => loadImg(urlId))
     return Promise.all(promiseArr)
   }
 
+  // 切割限定长度的数组，作为第一次请求的内容
   const promiseArr = urlIdsCopy.splice(0, limit).map(urlId => loadImg(urlId))
 
   urlIdsCopy.reduce((prevPromise, urlId) => prevPromise
@@ -14,6 +17,7 @@ const loadByLimit = (urlIds, loadImg, limit) => {
       console.log(error)
     })
     .then(resolvedId => {
+      // 请求成功之后，删除已完成请求并添加新的请求
       let resolvedIdPosition = promiseArr.findIndex(id => resolvedId === id)
       promiseArr.splice(resolvedIdPosition, 1)
       promiseArr.push(loadImg(urlId))
